8d62c8003c04f31aa37fc2e26c5be11d90e16560
Josh Rolstad <jrolstad@gmail.com>
2015-02-05 07:35:59 -0800

    Adding performance tests

---
+++ buffered_appender.tests/PerformanceTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
+using NUnit.Framework;
+
+namespace buffered_appender.tests
+{
+    [TestFixture]
+    public class PerformanceTests
+    {
+        [SetUp]
+        public void BeforeEach()
+        {
+            MessageSink.ClearLogs();
+        }
+
+        [Test]
+        [TestCase(5,1000,AppenderType.Asynchronous)]
+        [TestCase(5,1000,AppenderType.Synchronous)]
+        [TestCase(1,1000,AppenderType.Asynchronous)]
+        [TestCase(1,1000,AppenderType.Synchronous)]
+        [TestCase(10,1000,AppenderType.Asynchronous)]
+        [TestCase(10,1000,AppenderType.Synchronous)]
+        public void SynchronousAppender_Send_MeasurePerformance(int bufferSize, int messageCount,AppenderType appenderType)
+        {
+            // Arrange
+            BufferingAppenderSkeleton appender;
+            if (appenderType == AppenderType.Synchronous)
+            {
+                appender = new MyBufferedAppender {BufferSize = bufferSize};
+            }
+            else
+            {
+                appender = new MyAsyncBufferedAppender { BufferSize = bufferSize };
+            }
+
+            appender.ActivateOptions();
+
+            ConfigureAppender(appender);
+
+            var logger = LogManager.GetLogger(this.GetType());
+
+            // Act
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            for (int i = 0; i < messageCount; i++)
+            {
+                logger.Info(Guid.NewGuid().ToString());
+            }
+            stopWatch.Stop();
+
+            StopApplication();
+
+            // Assert
+            if(MessageSink.Logs.Count != messageCount)
+            {
+                // Give it time to finish sending
+                Thread.Sleep(2000);    
+            }
+
+            Assert.That(MessageSink.Logs.Count,Is.EqualTo(messageCount),"Not all the messages were sent!");
+
+            Console.WriteLine("BufferSize:{0}|MessageCount:{1}|Time(seconds):{2}",bufferSize,messageCount,stopWatch.Elapsed.TotalSeconds);
+            
+        }
+
+        public enum AppenderType
+        {
+            Synchronous,
+            Asynchronous
+        }
+
+        private static void StopApplication()
+        {
+            LogManager.Shutdown();
+        }
+
+        private static void ConfigureAppender(BufferingAppenderSkeleton appender)
+        {
+            var hierarchy = (Hierarchy)LogManager.GetRepository();
+            hierarchy.Root.Level = Level.Info;
+            hierarchy.Configured = true;
+
+            hierarchy.Root.AddAppender(appender);
+        }
+    }
+}
